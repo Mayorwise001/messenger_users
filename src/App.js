4e44574f476a448b7c9b53e9acdd502c30fa3b3b
mayorwise001 <tomosorijoseph@yahoo.com>
2024-08-05 04:56:33 +0100

    updated protected route

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
 
       <Router>
             <Routes>
-            <Route path="/" element={<ProtectedRoute />}>
+            {/* <Route path="/" element={<ProtectedRoute />}>
                     <Route index element={<Home />} />
                     <Route/>
-                    </Route>
+                    </Route> */}
                 <Route path='/' element={<Home/>}/>
                 <Route path='/sign-up' element={<SignUpForm/>}/>
                 <Route path='/login' element={<Login/>}/>
